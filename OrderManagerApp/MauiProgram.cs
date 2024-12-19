d4b867f53c982ccf87351346b2b337162e2103b3
Szidor Patrik <patrikp987@gmail.com>
2024-12-19 17:50:44 +0100

    Created Bottom navbar and added mudblazor services

--- OrderManagerApp/MauiProgram.cs
+++ OrderManagerApp/MauiProgram.cs
@@ -1,20 +1,34 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using DatabaseLibrary;
+using Microsoft.Extensions.Logging;
+using MudBlazor;
+using MudBlazor.Services;
 
 namespace OrderManagerApp;
 
-public static class MauiProgram
-{
-	public static MauiApp CreateMauiApp()
-	{
+public static class MauiProgram {
+	public static MauiApp CreateMauiApp() {
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
+			.ConfigureFonts(fonts => {
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
 
+		builder.Services.AddLocalization();
+		builder.Services.AddDbContext<Context>();
+
 		builder.Services.AddMauiBlazorWebView();
+		builder.Services.AddMudServices(config => {
+			config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomEnd;
+
+			config.SnackbarConfiguration.PreventDuplicates = true;
+			config.SnackbarConfiguration.ShowCloseIcon = true;
+			config.SnackbarConfiguration.VisibleStateDuration = 2500;
+			config.SnackbarConfiguration.HideTransitionDuration = 250;
+			config.SnackbarConfiguration.ShowTransitionDuration = 250;
+			config.SnackbarConfiguration.SnackbarVariant = Variant.Outlined;
+		});
+
 
 #if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
