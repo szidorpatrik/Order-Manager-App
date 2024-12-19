4614355597de27b2563e66a022156f270b772a07
Törőcsik Ádám <toarexer@gmail.com>
2024-12-19 15:22:53 +0100

    Added Empty Db Context

---
+++ DatabaseLibrary/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseLibrary;
+
+public sealed class Context : DbContext {
+	public const string FileName = "OrderManagerApp.db3";
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+		if (optionsBuilder.IsConfigured)
+			return;
+
+		string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), FileName);
+		optionsBuilder.UseSqlite($"Filename={path}");
+
+#if DEBUG
+		optionsBuilder.EnableSensitiveDataLogging();
+#endif
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder) {
+		
+	}
+}
