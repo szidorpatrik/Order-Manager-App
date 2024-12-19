4b231c6120213f6474f64db7f5d1f77535c8c028
Szidor Patrik <patrikp987@gmail.com>
2024-12-19 15:51:07 +0100

    Replaced ICollection with List

--- DatabaseLibrary/Models/Order.cs
+++ DatabaseLibrary/Models/Order.cs
@@ -7,7 +7,7 @@ public class Order
 {
     public int Id { get; set; }
     public int OrderNumber { get; set; }
-    public ICollection<OrderItem> OrderItems { get; set; }
+    public List<OrderItem> OrderItems { get; set; } = [];
     public DateTime DateCreated { get; set; }
     public DateTime DateStart { get; set; }
     public DateTime DateEnd { get; set; }
