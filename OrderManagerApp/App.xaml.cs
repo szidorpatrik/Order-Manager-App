70d4820d2a29ddc3f064eee787a857d743a3e246
Szidor Patrik <patrikp987@gmail.com>
2024-12-21 17:39:26 +0100

    Fixed the layout and changed theme colors

--- OrderManagerApp/App.xaml.cs
+++ OrderManagerApp/App.xaml.cs
@@ -1,10 +1,30 @@
-﻿namespace OrderManagerApp;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
 
-public partial class App : Application
-{
-	public App()
-	{
+namespace OrderManagerApp;
+
+public partial class App : Application {
+	public App() {
 		InitializeComponent();
+
+#if WINDOWS
+        int WindowWidth = 800;
+        int WindowHeight = 1200;
+
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            nint windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+        });
+#endif
 
 		MainPage = new MainPage();
 	}
--- OrderManagerApp/MauiProgram.cs
+++ OrderManagerApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using DatabaseLibrary;
+﻿using CommunityToolkit.Maui;
+using DatabaseLibrary;
 using Microsoft.Extensions.Logging;
 using MudBlazor;
 using MudBlazor.Services;
@@ -10,6 +11,7 @@ public static class MauiProgram {
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
 			.ConfigureFonts(fonts => {
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
