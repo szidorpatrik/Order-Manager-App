5c44d411892d564952b66737739798c07b65b0c3
Szidor Patrik <patrikp987@gmail.com>
2024-12-24 22:07:14 +0100

    Replaced sqlite with LiteDB and removed EntityFramework

---
+++ DatabaseLibrary/LiteDBService.cs
@@ -0,0 +1,28 @@
+﻿using DatabaseLibrary.Models;
+using Languages;
+using LiteDB;
+using System;
+using System.IO;
+
+namespace DatabaseLibrary {
+	using Microsoft.Extensions.Localization;
+
+	public class LiteDbService {
+		private readonly string fileName = "OrderManagerApp.db";
+		private readonly string _databasePath;
+		private readonly LiteDatabase _database;
+		protected readonly IStringLocalizer Localizer;
+
+		public LiteDbService(IStringLocalizer<OrderManagerAppLanguages> localizer, string? databasePath = null) {
+			Localizer = localizer;
+			_databasePath = Path.Combine(databasePath ?? Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), fileName);
+			_database = new LiteDatabase(_databasePath);
+		}
+
+		public ILiteCollection<Item> Items => _database.GetCollection<Item>("Items");
+		public ILiteCollection<Order> Orders => _database.GetCollection<Order>("Orders");
+		public ILiteCollection<OrderItem> OrderItems => _database.GetCollection<OrderItem>("OrderItems");
+
+		public LiteDatabase GetDatabase() => _database;
+	}
+}
--- DatabaseLibrary/Models/OrderItem.cs
+++ DatabaseLibrary/Models/OrderItem.cs
@@ -1,9 +1,9 @@
 ﻿namespace DatabaseLibrary.Models;
 
 public class OrderItem {
-	public int  Id       { get; set; }
-	public Item Item     { get; set; }
-	public int  Quantity { get; set; }
+	public int Id { get; set; }
+	public Item Item { get; set; }
+	public int Quantity { get; set; }
 
 	public override string ToString() => $"{Item.Name} ({Quantity})";
 }
