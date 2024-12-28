933ec39a78aaa8ef9888a8896f2ce617e5de1e58
Szidor Patrik <patrikp987@gmail.com>
2024-12-28 23:29:50 +0100

    Made Quantity nullable

--- DatabaseLibrary/Models/OrderItem.cs
+++ DatabaseLibrary/Models/OrderItem.cs
@@ -3,7 +3,7 @@
 public class OrderItem {
 	public int Id { get; set; }
 	public Item Item { get; set; }
-	public int Quantity { get; set; }
+	public int? Quantity { get; set; } = 0;
 
 	public override string ToString() {
 		if (Item == null) {
