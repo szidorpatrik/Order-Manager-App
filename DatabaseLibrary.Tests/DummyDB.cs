280343a22aa6ec453777d68e94010ed194621456
Szidor Patrik <patrikp987@gmail.com>
2025-01-18 14:53:07 +0100

    Added unit tests for untested methods

---
+++ DatabaseLibrary.Tests/DummyDB.cs
@@ -0,0 +1,157 @@
+﻿using DatabaseLibrary.Models;
+
+namespace DatabaseLibrary.Tests;
+
+using System.Collections;
+
+internal static class DummyDb {
+	internal static double[] GetItemPrices() {
+		return [4.99, 8.99, 9.99, 14.99, 19.99, 24.99, 49.99];
+	}
+
+	internal static IEnumerable<Order> GetOrders() {
+		double[] itemPrices = GetItemPrices();
+
+		Order order1 = new() {
+			OrderNumber = 10,
+			DateCreated = DateTime.Now,
+			DateStart = DateTime.Now,
+			DateEnd = DateTime.Now.AddHours(1),
+			IsCompleted = true,
+			IsCanceled = false,
+			OrderItems = [
+				new OrderItem() {
+					Id = 1,
+					Quantity = 1,
+					Item = new Item() {
+						Id = 1,
+						Name = "Test Item",
+						Price = itemPrices[0],
+					},
+				}
+			]
+		};
+
+		Order order2 = new() {
+			OrderNumber = 11,
+			DateCreated = DateTime.Now,
+			DateStart = DateTime.Now,
+			DateEnd = DateTime.Now.AddHours(1),
+			IsCompleted = true,
+			IsCanceled = false,
+			OrderItems = [
+				new OrderItem() {
+					Id = 2,
+					Quantity = 1,
+					Item = new Item() {
+						Id = 2,
+						Name = "Test Item",
+						Price = itemPrices[1],
+					}
+				}
+			]
+		};
+
+		Order order3 = new() {
+			OrderNumber = 12,
+			DateCreated = DateTime.Now,
+			DateStart = DateTime.Now,
+			DateEnd = DateTime.Now.AddHours(1),
+			IsCompleted = true,
+			IsCanceled = false,
+			OrderItems = [
+				new OrderItem() {
+					Id = 3,
+					Quantity = 1,
+					Item = new Item() {
+						Id = 3,
+						Name = "Test Item",
+						Price = itemPrices[2]
+					}
+				}
+			]
+		};
+
+		Order orderCanceled1 = new() {
+			OrderNumber = 20,
+			DateCreated = DateTime.Now,
+			DateStart = DateTime.Now,
+			DateEnd = DateTime.Now.AddHours(1),
+			IsCompleted = false,
+			IsCanceled = true,
+			OrderItems = [
+				new OrderItem() {
+					Id = 4,
+					Quantity = 1,
+					Item = new Item() {
+						Id = 4,
+						Name = "Test Item",
+						Price = itemPrices[3],
+					}
+				}
+			]
+		};
+
+		Order orderCanceled2 = new() {
+			OrderNumber = 21,
+			DateCreated = DateTime.Now,
+			DateStart = DateTime.Now,
+			DateEnd = DateTime.Now.AddHours(1),
+			IsCompleted = false,
+			IsCanceled = true,
+			OrderItems = [
+				new OrderItem() {
+					Id = 5,
+					Quantity = 1,
+					Item = new Item() {
+						Id = 5,
+						Name = "Test Item",
+						Price = itemPrices[4],
+					}
+				}
+			]
+		};
+
+		Order orderPending1 = new() {
+			OrderNumber = 30,
+			DateCreated = DateTime.Now,
+			DateStart = DateTime.Now,
+			DateEnd = DateTime.Now.AddHours(1),
+			IsCompleted = false,
+			IsCanceled = false,
+			OrderItems = [
+				new OrderItem() {
+					Id = 6,
+					Quantity = 1,
+					Item = new Item() {
+						Id = 6,
+						Name = "Test Item",
+						Price = itemPrices[5],
+					}
+				}
+			]
+		};
+
+		Order orderPending2 = new() {
+			OrderNumber = 31,
+			DateCreated = DateTime.Now,
+			DateStart = DateTime.Now,
+			DateEnd = DateTime.Now.AddHours(1),
+			IsCompleted = false,
+			IsCanceled = false,
+			OrderItems = [
+				new OrderItem() {
+					Id = 7,
+					Quantity = 1,
+					Item = new Item() {
+						Id = 7,
+						Name = "Test Item",
+						Price = itemPrices[6],
+					}
+				}
+			]
+		};
+
+		return [order1, order2, order3, orderCanceled1, orderCanceled2, orderPending1, orderPending2];
+	}
+}
--- DatabaseLibrary.Tests/OrderTests.cs
+++ DatabaseLibrary.Tests/OrderTests.cs
@@ -74,5 +74,130 @@ namespace DatabaseLibrary.Tests {
 			LiteDbService.AddOrder(order);
 			Assert.ThrowsException<InvalidOperationException>(() => { LiteDbService.AddOrder(order2); });
 		}
+
+		[TestMethod]
+		public void UncompleteOrder_ShouldBePending() {
+			Order order = new() {
+				OrderNumber = 999,
+				DateCreated = DateTime.Now,
+				DateStart = DateTime.Now,
+				DateEnd = DateTime.Now.AddHours(1),
+				IsCompleted = false,
+				IsCanceled = false
+			};
+
+			Assert.AreEqual(true, order.IsPending);
+		}
+
+		[TestMethod]
+		public void GetCompletedOrders_ShouldReturnAllCompletedOrders() {
+			var orders = DummyDb.GetOrders();
+
+			foreach (Order order in orders) {
+				LiteDbService.AddOrder(order);
+			}
+
+			var retrievedOrders = LiteDbService.GetCompletedOrders();
+			Assert.IsNotNull(retrievedOrders);
+			Assert.AreEqual(3, retrievedOrders.Count);
+		}
+
+		[TestMethod]
+		public void GetPendingOrders_ShouldReturnAllPendingOrders() {
+			var orders = DummyDb.GetOrders();
+
+			foreach (Order order in orders) {
+				LiteDbService.AddOrder(order);
+			}
+
+			var retrievedOrders = LiteDbService.GetPendingOrders();
+			Assert.IsNotNull(retrievedOrders);
+			Assert.AreEqual(2, retrievedOrders.Count);
+		}
+
+		[TestMethod]
+		public void GetCanceledOrders_ShouldReturnAllCanceledOrders() {
+			var orders = DummyDb.GetOrders();
+
+			foreach (Order order in orders) {
+				LiteDbService.AddOrder(order);
+			}
+
+			var retrievedOrders = LiteDbService.GetCanceledOrders();
+			Assert.IsNotNull(retrievedOrders);
+			Assert.AreEqual(2, retrievedOrders.Count);
+		}
+
+		[TestMethod]
+		public void GetOrderByOrderNumber_ShouldReturnOrderNumber() {
+			var orders = DummyDb.GetOrders();
+
+			foreach (Order order in orders) {
+				LiteDbService.AddOrder(order);
+			}
+
+			int[] numbers = [10, 12, 21, 30, 31];
+			foreach (int number in numbers) {
+				Order? order = LiteDbService.GetOrderByOrderNumber(number);
+				Assert.IsNotNull(order);
+				Assert.AreEqual(number, order.OrderNumber);
+			}
+		}
+
+		[TestMethod]
+		public void GetOrders_ShouldReturnAllOrders() {
+			var orders = DummyDb.GetOrders();
+
+			foreach (Order order in orders) {
+				LiteDbService.AddOrder(order);
+			}
+
+			var retrievedOrders = LiteDbService.GetOrders();
+			Assert.IsNotNull(retrievedOrders);
+			Assert.AreEqual(orders.Count(), retrievedOrders.Count);
+		}
+
+		[TestMethod]
+		public void GetOrdersCount_ShouldReturnAllOrdersCount() {
+			var orders = DummyDb.GetOrders();
+			foreach (Order order in orders) {
+				LiteDbService.AddOrder(order);
+			}
+
+			Assert.AreEqual(orders.Count(), LiteDbService.GetOrdersCount());
+		}
+
+		[TestMethod]
+		public void GetTotalPrice_ShouldReturnNotCanceledOrdersTotalPrice() {
+			var orders = DummyDb.GetOrders();
+			foreach (Order order in orders) {
+				LiteDbService.AddOrder(order);
+			}
+
+			var totalPrice = (double)orders.Where(x => !x.IsCanceled).Sum(o => o.OrderItems.Select(oi => oi.Item.Price).Sum());
+			Assert.AreEqual(totalPrice, LiteDbService.GetTotalPrice());
+		}
+
+		[TestMethod]
+		public void GetTotalPriceOfCompleted_ShouldReturnCompletedOrdersTotalPrice() {
+			var orders = DummyDb.GetOrders();
+			foreach (Order order in orders) {
+				LiteDbService.AddOrder(order);
+			}
+
+			var totalPrice = (double)orders.Where(x => x.IsCompleted).Sum(o => o.OrderItems.Select(oi => oi.Item.Price).Sum());
+			Assert.AreEqual(totalPrice, LiteDbService.GetTotalPriceOfCompleted());
+		}
+
+		[TestMethod]
+		public void GetTotalPriceOfPending_ShouldReturnPendingOrdersTotalPrice() {
+			var orders = DummyDb.GetOrders();
+			foreach (Order order in orders) {
+				LiteDbService.AddOrder(order);
+			}
+
+			var totalPrice = (double)orders.Where(x => x.IsPending).Sum(o => o.OrderItems.Select(oi => oi.Item.Price).Sum());
+			Assert.AreEqual(totalPrice, LiteDbService.GetTotalPriceOfPending());
+		}
 	}
 }
